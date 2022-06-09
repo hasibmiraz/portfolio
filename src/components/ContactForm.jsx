1f6a373ef684e55ea19def13464b76ae56f3f8ed
hasibmiraz <hasibmiraz09@gmail.com>
2022-06-10 00:47:00 +0600

    Contact form function done

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ContactForm = () => {
   return (
     <div className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/ea27974d-2487-4cae-bbe4-491bbf94a8f6"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Get in touch
@@ -17,25 +21,22 @@ const ContactForm = () => {
           name="name"
           placeholder="Name"
           className="my-4 p-2 bg-[#ccd6f6]"
+          required
         />
         <input
           type="email"
           name="email"
           placeholder="Email"
           className="my-4 p-2 bg-[#ccd6f6]"
+          required
         />
-        {/* <input
-          type="text"
-          name="subject"
-          placeholder="Subject"
-          className="my-4 p-2 bg-[#ccd6f6]"
-        /> */}
         <textarea
           type="text"
           name="message"
           placeholder="Message"
           rows={10}
           className="my-4 p-2 bg-[#ccd6f6]"
+          required
         ></textarea>
         <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center duration-200 hover:rounded">
           Send Email
