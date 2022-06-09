e65f4cd1960050a60e5eb66edeb80380131ad9db
hasibmiraz <hasibmiraz09@gmail.com>
2022-06-09 20:01:08 +0600

    Header menu working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -8,6 +9,36 @@ const Header = () => {
           Md Hasibul Alam
         </h2>
       </div>
+
+      {/* Desktop menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      {/* Desktop menu end */}
+
+      {/* Hamburger menu */}
+      <div className="md:hidden z-10">
+        <FaBars className="w-6 h-6" />
+      </div>
+      {/* Hamburger menu end */}
+
+      {/* Mobile Menu */}
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a193c] flex flex-col justify-center items-center">
+        <li className="py-5 text-4xl">Home</li>
+        <li className="py-5 text-4xl">About</li>
+        <li className="py-5 text-4xl">Skills</li>
+        <li className="py-5 text-4xl">Projects</li>
+        <li className="py-5 text-4xl">Contact</li>
+      </ul>
+      {/* Mobile Menu end */}
+
+      {/* Social */}
+      <div className="hidden"></div>
+      {/* Social end */}
     </div>
   );
 };
