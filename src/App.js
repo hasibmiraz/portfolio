e0deb0f1b73e91266c887ce0d0a918ff08637622
hasibmiraz <hasibmiraz09@gmail.com>
2022-06-09 21:17:50 +0600

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Header from './components/Header';
 import Home from './components/Home';
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 }
